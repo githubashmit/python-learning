b96da78ee78184c2004f4f6e2af83b92fdb77a0f
Ashmit <ashmitgithub@gmail.com>
2022-11-11 13:02:06 +0530

    11-11-2022

---
+++ INT108/Theory/fileWriting.py
@@ -0,0 +1,15 @@
+f=open("ashmit.txt","rt")
+print(f.readline())
+print(f.readline())
+# content = f.read()
+
+# for line in f:
+#     print(line,end="")
+# print(content)
+# content =f.read(344)
+# print("1",content)
+#
+# content =f.read(344)
+# print("2",content)
+
+f.close()
